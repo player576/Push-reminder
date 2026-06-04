732ffbd807d3d98f4b5c66b7d7b518d5fa769b02
player576 <danilserverdpa2026@gmail.com>
2026-06-04 09:38:56 +0300

    Implement push event listener in Service Worker
    Added push event listener to handle notifications.

--- sw.js
+++ sw.js
@@ -1,9 +1,28 @@
-// Этот код выполняется в фоновом потоке браузера
+// Фоновый Service Worker
 self.addEventListener('install', (event) => {
-    console.log('Service Worker установлен');
+    console.log('Service Worker: Установлен');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker активирован');
+    console.log('Service Worker: Активирован');
+    return self.clients.claim();
+});
+
+// Слушатель на случай, если пуш придёт через стандартный Web-канал
+self.addEventListener('push', (event) => {
+    if (event.data) {
+        const data = event.data.json();
+        const options = {
+            body: data.body,
+            icon: 'https://via.placeholder.com/128',
+            vibrate: [200, 100, 200],
+            data: {
+                dateOfArrival: Date.now()
+            }
+        };
+        event.waitUntil(
+            self.registration.showNotification(data.title, options)
+        );
+    }
 });
